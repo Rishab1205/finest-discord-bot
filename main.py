960940b7aa4c232e3bba63383154ea49708127f6
Rishab1205 <145602798+Rishab1205@users.noreply.github.com>
2026-02-25 09:46:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+import re
 # ================= FREE PACK STORAGE =================
 
 app = Flask('')
