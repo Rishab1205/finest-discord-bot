38d44ef47dc26b9f9814c632ede11ad20bb41245
Rishab1205 <145602798+Rishab1205@users.noreply.github.com>
2026-02-25 21:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -682,7 +682,7 @@ async def send_payment_dm(member, ticket_channel):
     await asyncio.sleep(1)  # delay for Discord DM handshake
     try:
         embed = discord.Embed(
-            title="<:sc_blue_arrow:1475937927854031073> Payment Confirmed!",
+            title="<a:sc_blue_arrow:1475937927854031073> Payment Confirmed!",
             description=(
                 f"Hey **{member.name}** <a:kami_crown:1214643796671733780>\n"
                 "Your purchase was successful!"
@@ -796,7 +796,7 @@ async def process_member(member):
 async def send_join_dm(member):
     try:
         embed = discord.Embed(
-            title="<:sc_blue_arrow:1475937927854031073> Welcome to VALID DC",
+            title="<a:sc_blue_arrow:1475937927854031073> Welcome to FINEST DC",
             description=(
                 f"Hey **{member.name}**, welcome aboard! <:vg12:1466375282154803220>\n\n"
                 "You're now part of a community built for gamers who respect:\n"
